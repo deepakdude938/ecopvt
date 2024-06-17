782bc9d0a0dab287e5996420d5b9a5ba2d81b6c9
deepakdude <deepakdude938@gmail.com>
2024-06-17 15:06:46 +0530

    Updated code

--- src/main/java/drivers/DriverFactory.java
+++ src/main/java/drivers/DriverFactory.java
@@ -1,7 +1,10 @@
 package drivers;
 
+import java.util.HashMap;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class DriverFactory {
@@ -10,7 +13,24 @@ public class DriverFactory {
 	{
 		if(browsername.equals("Chrome")) 
 		{
-			driver = new ChromeDriver();
+			String basePath = System.getProperty("user.dir");
+			String path =basePath+"Eco_PVT\\downloadfiles";
+			HashMap<String, Object> chromePrefs = new HashMap<>();
+			chromePrefs.put("profile.default_content_settings.popups", 0);
+			chromePrefs.put("download.default_directory", path);
+
+			ChromeOptions options = new ChromeOptions();
+			options.setExperimentalOption("prefs", chromePrefs);
+			driver = new ChromeDriver(options);
+			
+			
+			
+	//		File fileCSV = new File(path+"smartsense.csv");
+		//	Assert.assertTrue(file.exists(), "File not downloaded successfully");
+			
+			//File filexlsx = new File(path+"smartsense.xlsx");
+			//Assert.assertTrue(file.exists(), "File not downloaded successfully");
+			
 		}
 		else if(browsername.equals("Edge"))
 		{
